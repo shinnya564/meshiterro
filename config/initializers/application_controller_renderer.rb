193b30e887a544c025763d44e84f023d82a39a1e
shinnya <65529573+shinnya564@users.noreply.github.com>
2020-06-10 20:03:07 +0900

    [update]meshiterro_chapter3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '979ab9cf48e51aa90f24812c997aa264fe0e7f8045ae220f434070acd7babcca2d18a1a766512319c5a30bc702b4d978e5f2ccf645c3c7fd606e86c5529749ed'
