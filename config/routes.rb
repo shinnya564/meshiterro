fe1a85e8660aca62c0082830aca22bbddd20acc3
shinnya <65529573+shinnya564@users.noreply.github.com>
2020-06-10 22:20:05 +0900

    [update]meshiterro_chapter5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
 
-  root 'post_images#index'
+  	devise_for :users
+  	root 'post_images#index'
 
-resources :post_images, only: [:new, :create, :index, :show] do
-    resource :favorites, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
-end
+	resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    	resource :favorites, only: [:create, :destroy]
+    	resources :post_comments, only: [:create, :destroy]
+	end
+
+	resources :users, only: [:show]
 
 end
